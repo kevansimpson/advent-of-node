afc923eba648f04a23edba7b797b20da13a99efe
Kevan Simpson <kevan.simpson@gmail.com>
2019-11-13 20:52:07 -0800

    Solution Day07,2015

---
+++ src/2015/day07.ts
@@ -0,0 +1,75 @@
+/**
+ * @module 2015_day07
+ */
+
+export function signalA(directions: string[]): number {
+  return buildCircuitMap(directions).calculate('a')
+}
+
+export function overrideSignalB(directions: string[]): number {
+  const cmap = buildCircuitMap(directions)
+  // override wire 'b'
+  cmap.valueMap = { 'b': 46065 }
+  return cmap.calculate('a')
+}
+
+export function buildCircuitMap(directions: string[]): CircuitMap {
+  const cmap: CircuitMap = new CircuitMap()
+  for (const dir of directions) {
+    const keyval = dir.split(/\s->\s/g)
+    cmap.circuits[keyval[1]] = keyval[0].split(/\s/g)
+  }
+  return cmap
+}
+
+class CircuitMap {
+  circuits: { [key: string]: string[] } = {}
+  valueMap: SignalMap = {}
+
+  calculate(wire: string): number {
+    const logic: string[] = this.circuits[wire]
+    if (!this.valueMap[wire]) {
+      if (!logic) {
+        return +wire
+      }
+      else {
+        switch (logic.length) {
+          case 1: // literal value OR undocumented variable
+            const val = Number.parseInt(logic[0])
+            if (Number.isNaN(val)) this.valueMap[wire] = this.calculate(logic[0])
+            else this.valueMap[wire] = val
+            break
+          case 2: // bitwise complement, the ~ operator is NOT appropriate
+            if ('NOT' === logic[0]) this.valueMap[wire] = (65535 - this.calculate(logic[1]))
+            break
+          case 3: // equation
+            switch (logic[1]) {
+              case 'AND':
+                const val2 = Number.parseInt(logic[2])
+                const val0 = this.calculate(logic[0])
+                if (Number.isNaN(val2)) {
+                  this.valueMap[wire] = val0 & this.calculate(logic[2])
+                } else {
+                  this.valueMap[wire] = val0 & val2
+                }
+                break
+              case 'OR':
+                this.valueMap[wire] = this.calculate(logic[0]) | this.calculate(logic[2])
+                break
+              case 'LSHIFT':
+                this.valueMap[wire] = this.calculate(logic[0]) << Number.parseInt(logic[2])
+                break
+              case 'RSHIFT':
+                this.valueMap[wire] = this.calculate(logic[0]) >>> Number.parseInt(logic[2])
+                break
+            }
+        }
+
+      }
+    }
+
+    return this.valueMap[wire]
+  }
+}
+
+type SignalMap = { [key: string]: number }
---
+++ test/2015/examples.2015.07.spec.ts
@@ -0,0 +1,27 @@
+import assert from 'assert';
+import 'mocha'
+import { buildCircuitMap } from '../../src/2015/day07'
+
+describe.only('2015 examples - Day 07', () => {
+  it('signal A', () => {
+    const input = [
+      '123 -> x',
+      '456 -> y',
+      'x AND y -> d',
+      'x OR y -> e',
+      'x LSHIFT 2 -> f',
+      'y RSHIFT 2 -> g',
+      'NOT x -> h',
+      'NOT y -> i'
+    ]
+    const cmap = buildCircuitMap(input)
+    assert.strictEqual(cmap.calculate('x'), 123)
+    assert.strictEqual(cmap.calculate('y'), 456)
+    assert.strictEqual(cmap.calculate('d'), 72)
+    assert.strictEqual(cmap.calculate('e'), 507)
+    assert.strictEqual(cmap.calculate('f'), 492)
+    assert.strictEqual(cmap.calculate('g'), 114)
+    assert.strictEqual(cmap.calculate('h'), 65412)
+    assert.strictEqual(cmap.calculate('i'), 65079)
+  })
+})
