5c7903fd935bfcb5c5b198409d85426e0efd4cb3
Kevan Simpson <kevan.simpson@gmail.com>
2019-11-16 22:33:03 -0800

    Solution to Day12,2015

---
+++ src/2015/day12.ts
@@ -0,0 +1,29 @@
+/**
+ * @module 2015_day11
+ */
+
+export function sumDocument(input: string, excluded: string[] = []): number {
+  return sumObject(JSON.parse(input), excluded)
+}
+
+function sumArray(array: any[], excluded: string[] = []): number {
+  return array.reduce((total, value) => total + sumObject(value, excluded), 0)
+}
+
+function sumObject(obj: any, excluded: string[] = []): number {
+  if (Array.isArray(obj)) return sumArray(obj, excluded)
+  else {
+    switch (typeof obj) {
+      case 'object':
+        let sum = 0
+        for (const key of Object.keys(obj)) {
+          if (excluded.indexOf(obj[key]) >= 0) return 0
+          else sum += sumObject(obj[key], excluded)
+        }
+        return sum
+      case 'number': return obj
+      case 'string': return Number.parseInt(obj) || 0
+    }
+  }
+  throw new Error(`sumObject => ${JSON.stringify(obj)}`)
+}
---
+++ test/2015/examples.2015.12.spec.ts
@@ -0,0 +1,23 @@
+import assert from 'assert';
+import 'mocha'
+import { sumDocument } from '../../src/2015/day12'
+
+describe.only('2015 examples - Day 12', () => {
+  it('sum document', () => {
+    assert.strictEqual(sumDocument('[1,2,3]'), 6)
+    assert.strictEqual(sumDocument('{"a":2,"b":4}'), 6)
+    assert.strictEqual(sumDocument('[[[3]]]'), 3)
+    assert.strictEqual(sumDocument('{"a":{"b":4},"c":-1}'), 3)
+    assert.strictEqual(sumDocument('{"a":[-1,1]}'), 0)
+    assert.strictEqual(sumDocument('[-1,{"a":1}]'), 0)
+    assert.strictEqual(sumDocument('[]'), 0)
+    assert.strictEqual(sumDocument('{}'), 0)
+  })
+
+  it('sum document w/ exclusions', () => {
+    assert.strictEqual(sumDocument('[1,2,3]', ['red']), 6)
+    assert.strictEqual(sumDocument('[1,{"c":"red","b":2},3]', ['red']), 4)
+    assert.strictEqual(sumDocument('{"d":"red","e":[1,2,3,4],"f":5}', ['red']), 0)
+    assert.strictEqual(sumDocument('[1,"red",5]', ['red']), 6)
+  })
+})
