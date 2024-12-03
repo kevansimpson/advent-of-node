ca8209e2beb2615933d6a7f52b7ad95c7a91c196
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2024-12-03 08:40:24 -0800

    move input file to resources dir

--- test/2024/answers.2024.01.spec.ts
+++ test/2024/answers.2024.01.spec.ts
@@ -6,7 +6,7 @@ import { solve } from '../../src/2024/day01'
 import * as d01 from '../../src/2024/day01.doc'
 
 describe('2024 answers - Day 01', () => {
-  const input = readLines(path.join(__dirname, 'input01.txt'))
+  const input = readLines(path.join(__dirname, '../resources/2024/input01.txt'))
   const result = solve(input)
   it('totalDifference', async () => assert.strictEqual(result.diff, d01.part1))
   it('similarityScore', async () => assert.strictEqual(result.score, d01.part2))
