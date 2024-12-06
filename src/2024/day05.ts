42eff6064e83d41f330d03ff88a0eb38b12ad6bb
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2024-12-06 07:46:44 -0800

    solution Day05, 2024

---
+++ src/2024/day05.ts
@@ -0,0 +1,72 @@
+/**
+ * @module 2024_day05
+ */
+
+export type SafetyManual = {
+  middleSum: number,
+  corrected: number
+}
+
+export function solve (input: string[]): SafetyManual {
+  let middleSum = 0
+  let corrected = 0
+  const rulesUpdates = new RulesUpdates(input)
+  rulesUpdates.updates.forEach(update => {
+    if (inOrder(update, rulesUpdates))
+      middleSum += update[Math.floor(update.length / 2)]
+    else
+      corrected += reorderUpdate(update, rulesUpdates)
+  })
+
+  return ({ middleSum: middleSum, corrected: corrected })
+}
+
+function inOrder(update: number[], rules: RulesUpdates): boolean {
+  for (let i = 0; i < update.length - 1; i++)
+    for (let j = (i + 1); j < update.length; j++)
+        if (rules.isBeforeUpdate(update, i, j))
+            return false
+
+  return true
+}
+
+function reorderUpdate(update: number[], rules: RulesUpdates): number {
+  update.sort((a, b) => {
+    if (rules.isBefore(b, a))
+      return -1
+    else
+      return 1
+  })
+  return update[Math.floor(update.length / 2)]
+}
+
+class RulesUpdates {
+  private before: Map<number, number[]>
+  public updates: number[][]
+
+  public constructor(input: string[]) {
+    this.before = new Map()
+    let i = 0
+    for (i; i < input.length; i++) {
+      if (input[i].length == 0)
+        break
+      const rule = input[i].split('|').map(n => Number(n))
+      if (!this.before.has(rule[0]))
+        this.before.set(rule[0], [])
+      this.before.get(rule[0])!.push(rule[1])
+    }
+  
+    // updates
+    this.updates = []
+    for (let j = ++i; j < input.length; j++)
+      this.updates.push(input[j].split(',').map(n => Number(n)))
+  }
+
+  public isBefore(i: number, j: number): boolean {
+    return !this.before.has(i) || !this.before.get(i)!.includes(j)
+  }
+
+  public isBeforeUpdate(update: number[], i: number, j: number): boolean {
+    return !this.before.has(update[i]) || !this.before.get(update[i])!.includes(update[j])
+  }
+}
---
+++ test/2024/answers.2024.05.spec.ts
@@ -0,0 +1,13 @@
+import assert from 'assert'
+import 'mocha'
+import path from 'path'
+import { readLines } from '../../src/helpers/input'
+import { solve } from '../../src/2024/day05'
+import * as d05 from '../../src/2024/day05.doc'
+
+describe('2024 answers - Day 05', () => {
+  const input = readLines(path.join(__dirname, '../resources/2024/input05.txt'))
+  const result = solve(input)
+  it('middle sum', async () => assert.strictEqual(result.middleSum, d05.part1))
+  it('corrected sum', async () => assert.strictEqual(result.corrected, d05.part2))
+})
