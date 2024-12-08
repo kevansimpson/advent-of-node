f804e25562ce9c4f544a91eda57c81ca4f1b91df
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2024-12-08 13:19:22 -0800

    solution Day07, 2024

---
+++ src/2024/day07.ts
@@ -0,0 +1,51 @@
+/**
+ * @module 2024_day07
+ */
+
+export type RopeBridge = {
+  calibration: number,
+  concatenation: number
+}
+
+type Operation = ((a: number, b: number) => number)
+
+
+const ADD_MULTIPLY: Operation[] = [(a: number, b: number) => a + b, (a: number, b: number) => a * b]
+const WITH_CONCAT: Operation[] = [...ADD_MULTIPLY, (a: number, b: number) => Number(`${a}${b}`)]
+
+export function solve (input: string[]): RopeBridge {
+  let calibration = 0
+  let concatenation = 0
+  input.forEach( equation => {
+    const values = Array.from(equation.matchAll(/(\d+)/g)).map(n => Number(n[0]))
+    if (canBeCalibrated(values, ADD_MULTIPLY))
+      calibration += values[0]
+    if (canBeCalibrated(values, WITH_CONCAT))
+      concatenation += values[0]
+  })
+
+  return ({ calibration: calibration, concatenation: concatenation })
+}
+
+function canBeCalibrated(values: number[], operations: Operation[]): boolean {
+  for (let i = 0; i < operations.length; i++) {
+    if (isCalibrated(values, 0, 1, i, operations))
+      return true
+  }
+
+  return false
+}
+
+function isCalibrated(values: number[], sum: number, index: number, op: number,
+                         operations: Operation[]): boolean {
+  if (sum == values[0] && index == values.length)
+    return true
+  else if (index < values.length) {
+    for (let i = 0; i < operations.length; i++) {
+      if (isCalibrated(values, operations[op](sum, values[index]), index + 1, i, operations))
+        return true
+    }
+  }
+
+  return false
+}
---
+++ test/2024/answers.2024.07.spec.ts
@@ -0,0 +1,14 @@
+import assert from 'assert'
+import 'mocha'
+import path from 'path'
+import { readLines } from '../../src/helpers/input'
+import { solve } from '../../src/2024/day07'
+import * as d07 from '../../src/2024/day07.doc'
+
+describe('2024 answers - Day 07', () => {
+  const input = readLines(path.join(__dirname, '../resources/2024/input07.txt'))
+  const result = solve(input)
+  
+  it('calibrations', async () => assert.strictEqual(result.calibration, d07.part1))
+  it('concatenations', async () => assert.strictEqual(result.concatenation, d07.part2))
+})
