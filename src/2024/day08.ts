6360ee1b390bc3326705ead01a8d07e557d76aab
Kevan Simpson Air1 <kevan.simpson@gmail.com>
2024-12-08 21:34:03 -0800

    solution Day08, 2024

---
+++ src/2024/day08.ts
@@ -0,0 +1,64 @@
+/**
+ * @module 2024_day08
+ */
+
+import { inGrid, moveXY, Point, toKey } from "../helpers/point"
+import { combinations } from "../helpers/util"
+
+export type Antinodes = {
+    count: number,
+    harmonics: number
+}
+
+export function solve (input: string[]): Antinodes {
+  const size = input.length
+  const scan: Map<string, Point[]> = scanAntennas(input)
+  const nodes = new Set<string>
+  const harmonics = new Set<string>
+
+  scan.forEach(antennas => {
+    const pairs = combinations(antennas, 2)
+    pairs.forEach(pair => {
+      const a = pair[0]
+      const b = pair[1]
+      harmonics.add(toKey(a))
+      harmonics.add(toKey(b))
+      const dx1 = a[0] - b[0]
+      const dy1 = a[1] - b[1]
+      const dx2 = b[0] - a[0]
+      const dy2 = b[1] - a[1]
+      let next1: Point = [a[0] + dx1, a[1] + dy1]
+      let next2: Point = [b[0] + dx2, b[1] + dy2]
+      if (inGrid(next1, size, size))
+        nodes.add(toKey(next1))
+      if (inGrid(next2, size, size))
+        nodes.add(toKey(next2))
+      while (inGrid(next1, size, size)) {
+        harmonics.add(toKey(next1))
+        next1 = moveXY(next1, dx1, dy1)
+      }
+      while (inGrid(next2, size, size)) {
+        harmonics.add(toKey(next2))
+        next2 = moveXY(next2, dx2, dy2)
+      }
+    })
+  })
+    
+  return ({ count: nodes.size, harmonics: harmonics.size })
+}
+
+function scanAntennas(input: string[]): Map<string, Point[]> {
+  const map = new Map()
+  for (let r = input.length - 1; r >= 0; r--) {
+    for (let c = 0; c < input.length; c++) {
+      const at = input[r][c]
+      if (at != '.') {
+        if (!map.has(at))
+          map.set(at, [])
+        map.get(at).push([c, r])
+      }
+    }
+  }
+
+  return map
+}
---
+++ test/2024/answers.2024.08.spec.ts
@@ -0,0 +1,14 @@
+import assert from 'assert'
+import 'mocha'
+import path from 'path'
+import { readLines } from '../../src/helpers/input'
+import { solve } from '../../src/2024/day08'
+import * as d08 from '../../src/2024/day08.doc'
+
+describe('2024 answers - Day 08', () => {
+  const input = readLines(path.join(__dirname, '../resources/2024/input08.txt'))
+  const result = solve(input)
+  
+  it('antinode count', async () => assert.strictEqual(result.count, d08.part1))
+  it('harmonic antinodes', async () => assert.strictEqual(result.harmonics, d08.part2))
+})
